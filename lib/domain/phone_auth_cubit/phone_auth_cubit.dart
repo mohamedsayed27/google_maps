596119efab93ff31ccf5de95088fa7b5824bd4c3
dell <mohamedsayed27>
2022-08-29 00:03:04 +0200

    in progress :)

--- lib/domain/phone_auth_cubit/phone_auth_cubit.dart
+++ lib/domain/phone_auth_cubit/phone_auth_cubit.dart
@@ -40,7 +40,7 @@ class PhoneAuthCubit extends Cubit<PhoneAuthState> {
     emit(PhoneNumberSubmittedState());
   }
 
-//commenntttt
+//commenntt
   Future<void> submitOtp(String otpCode) async {
     PhoneAuthCredential credential = PhoneAuthProvider.credential(
         verificationId: verificationId, smsCode: otpCode);
